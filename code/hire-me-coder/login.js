858df14e814c4085192409e3f5bf1c30388ca7d2
Yuedong Wang <a1775906@student.adelaide.edu.au>
2019-08-30 02:31:53 +0930

    login

---
+++ code/hire-me-coder/login.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
+var firebase = require('firebase');
+var logger = require('morgan')
+
+var login = express();
+
+
+
+//EJS layout
+login.use(expressLayouts);
+login.set('view engine', 'ejs');
+
+login.use(express.static('views'))
+
+//Routes
+login.use('/users', require('./routes/users'));
+
+
+
+// // Admin login
+// var admin = require('firebase-admin')
+// var serviceAccount = require('./sep-2019-pg5-39ccb-firebase-adminsdk-yivcu-073cc6edb0.json')
+// var firebaseAdmin = admin.initializeApp({
+//   credential:admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://sep-2019-pg5-39ccb.firebaseio.com'
+// });
+
+
+
+// // Create authentication.middleware
+// const isAuthenticated = (req, res, next) => {
+//   // check if user is logged in
+//   // if they are, attach them to the request and call next
+//   // if not, send to the login page with a message"login"
+//   let idToken;
+//   if(req.hearders.authorization && req.headers.authorization.startsWith('Bearer ')){
+//     return next();
+//     // idToken = req.hearders.authorization.split('Bearer ')[1];
+//   } else {
+//     console.error('No token found')
+//     return res.status(403).json({ error: 'Unauthorized'});
+//   }
+
+  // admin.auth().verifyIdToken(idToken)
+  //   .then(decodedToken => {
+  //     req.user = decodedToken;
+  //     return firebase.collection('users')
+  //       .where('userId', '==', req.user.uid)
+  //       .limit(1)
+  //       .get();
+  //   })
+  //   .then(data => {
+  //     req.user.handle = data.docs[0].data().handle;
+  //     return next();
+  //   })
+// }
+
+
+
+
+var PORT = process.env.PORT || 8080;
+
+login.listen(PORT, console.log(`Server started on port ${PORT}`));
+
---
+++ code/hire-me-coder/views/js/auth.js
@@ -0,0 +1,89 @@
+// // Firebase App (the core Firebase SDK) is always required and must be listed first
+// import * as firebase from "firebase/app";
+
+// // Add the Firebase products that you want to use
+// import "firebase/auth";
+// import "firebase/database";
+// import "firebase/firestore";
+
+
+(function() {
+
+    
+    
+    
+    // Web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyC49UseYKNdFnCuUV7Fpq-4ZjkIEL1IDCI",
+        authDomain: "sep-2019-pg5-39ccb.firebaseapp.com",
+        databaseURL: "https://sep-2019-pg5-39ccb.firebaseio.com",
+        projectId: "sep-2019-pg5-39ccb",
+        storageBucket: "sep-2019-pg5-39ccb.appspot.com",
+        messagingSenderId: "989504657582",
+        appId: "1:989504657582:web:2d2dc47a72c6ae77"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+    
+    // Get elements
+    const txtEmail = document.getElementById('email');
+    const txtPassword = document.getElementById('password');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnLogout = document.getElementById('btnLogout');
+    const btnAdmin = document.getElementById('btnAdmin');
+    const loginGroup = document.getElementById('login-group');
+    const logoutGroup = document.getElementById('logout-group');
+    
+    // Add login event
+    btnLogin.addEventListener('click', e => {
+        // Get email and pwd
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+        const auth = firebase.auth();
+        
+        // Sign in
+        const promise = auth.signInWithEmailAndPassword(email, password);
+        promise.catch(e => console.log(e.message));
+    }); 
+    
+    
+    //Add logout event
+    btnLogout.addEventListener('click', e=> {
+        firebase.auth().signOut();
+    });
+
+
+    //Add go admin
+    btnAdmin.addEventListener('click', e=>{
+        login
+        res.redirect('http://google.com')
+    })
+
+
+    
+    
+    
+    
+    // Add a realtime listner
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            console.log('logged in');
+            
+            loginGroup.style.display="none";
+            logoutGroup.style.display="block";
+            // return res.redirect('/admin');
+            
+        } else {
+            console.log('not logged in');
+            
+            logoutGroup.style.display="none";
+            loginGroup.style.display="block";
+            
+        }
+    
+    });
+    
+    }());
+
+
