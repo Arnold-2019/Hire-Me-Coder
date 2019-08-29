37ade8824e08b89d0624f257239ddd7492286e43
Ying Wang <a1733805@student.adelaide.edu.au>
2019-08-29 16:54:00 +0930

    Email Invitation module submit.

--- code/hire-me-coder/app.js
+++ code/hire-me-coder/app.js
@@ -7,6 +7,42 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'hiremecoder.pg5@gmail.com',
+    pass: 'Sep_group5'
+  }
+});
+
+var mailAddress;
+var mailOptions = {
+  from: 'hiremecoder.pg5@gmail.com',
+  to: 'ying86y6@gmail.com',
+  subject: 'HireMeCoder Invitation!',
+  text: 'Wellcome to Hire Me Coder\n\n' + 
+        'We have kindly created an account for you, and with this account ' +
+        'you can access your invitation assgnments.\n\n' +
+        'Your Account name is your email address and the defult initial Password ' +
+        'is attached below. You should update your Password after logging in.\n\n' +
+        'Account name: youremailaddress@xxx.gmail\n' +
+        'Password:\nHiremecoder_2019\n\n' +
+        'Please click the link below to login to Hime Me Coder\n\n' +
+        '\" THE LINK TO LOGIN TO HIRE ME CODER \"    \n\n' +
+        'Best regards,\n' +
+        'Hire Me Coder Team'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+
 var app = express();
 
 // view engine setup
