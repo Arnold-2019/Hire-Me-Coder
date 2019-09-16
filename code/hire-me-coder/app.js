c01ee27ee86be845af5113be20dad770fc98fc80
Ying Wang <ying.wang01@student.adelaide.edu.au>
2019-09-16 12:01:38 +0930

    Assessment send

--- code/hire-me-coder/app.js
+++ code/hire-me-coder/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var emailRouter = require('./routes/email');
+var invitationRouter = require('./routes/assessment');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/email', emailRouter);
+app.use('/assessment', invitationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
