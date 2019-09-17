d6963c034597897b2c024fc47cb51aa28175babf
Ying Wang <ying.wang01@student.adelaide.edu.au>
2019-09-17 14:49:07 +0930

    Improved the UI Implemented the time picker generate the email message

--- code/hire-me-coder/public/javascripts/assessment.js
+++ code/hire-me-coder/public/javascripts/assessment.js
@@ -33,49 +33,49 @@ function getTest() {
     });
 }
 
-$('#previewButton').click(() => {
-    console.log('hello');
+// $('#previewButton').click(() => {
+//     console.log('hello');
     
-    $.ajax({
-        url: 'preview/',
-        type: 'GET',
-        dataType: 'text',
-        success: (data) => {
-            console.log('You received s ome data!', data);
-            var mails = $('#inputEmailBox').val();
-            var tests = $('#inputAssBox').val();
-            var due = $('#inputDateBox').val();
-            var time = $('#inputTimeBox').val();
-            var p1 = 'Wellcome,';
-            var p2 = 'We have kindly created an account for you, and with this account ' +
-                     'you can access your assessment form.';
-            var p3 = 'Please submit your solutions by the ' + due + time;
-            var p4 = 'Your Account name is your email address and the defult initial password ' +
-                     'is attached below. Realy recommend to update your Password after logging in.';
-            var p5 = 'User name: ' + mails;
-            var p6 = 'Password: hjhHI23jJK';
-            var p7 = 'Please click the link below to login to your account.';
-            var p8 = 'http://the.link.to.login.maptek';
-            var p9 = 'Best regards,';
-            var p10= 'MapTek Team';
+//     $.ajax({
+//         url: 'preview/',
+//         type: 'GET',
+//         dataType: 'text',
+//         success: (data) => {
+//             console.log('You received s ome data!', data);
+//             var mails = $('#inputEmailBox').val();
+//             var tests = $('#inputAssBox').val();
+//             var due = $('#inputDateBox').val();
+//             var time = $('#inputTimeBox').val();
+//             var p1 = 'Wellcome,';
+//             var p2 = 'We have kindly created an account for you, and with this account ' +
+//                      'you can access your assessment form.';
+//             var p3 = 'Please submit your solutions by the ' + due + time;
+//             var p4 = 'Your Account name is your email address and the defult initial password ' +
+//                      'is attached below. Realy recommend to update your Password after logging in.';
+//             var p5 = 'User name: ' + mails;
+//             var p6 = 'Password: hjhHI23jJK';
+//             var p7 = 'Please click the link below to login to your account.';
+//             var p8 = 'http://the.link.to.login.maptek';
+//             var p9 = 'Best regards,';
+//             var p10= 'MapTek Team';
 
-            $('#mails').html('Send to: ' + mails);
-            $('#tests').html('Test name: ' + tests);
-            $('#due').html('Due date: ' + due);
-            $('#template').html('Email template: ');
-            $('#p1').html(p1);
-            $('#p2').html(p2);
-            $('#p3').html(p3);
-            $('#p4').html(p4);
-            $('#p5').html(p5);
-            $('#p6').html(p6);
-            $('#p7').html(p7);
-            $('#p8').html(p8);
-            $('#p9').html(p9);
-            $('#p10').html(p10);
-        }
-    });
-});
+//             $('#mails').html('Send to: ' + mails);
+//             $('#tests').html('Test name: ' + tests);
+//             $('#due').html('Due date: ' + due);
+//             $('#template').html('Email template: ');
+//             $('#p1').html(p1);
+//             $('#p2').html(p2);
+//             $('#p3').html(p3);
+//             $('#p4').html(p4);
+//             $('#p5').html(p5);
+//             $('#p6').html(p6);
+//             $('#p7').html(p7);
+//             $('#p8').html(p8);
+//             $('#p9').html(p9);
+//             $('#p10').html(p10);
+//         }
+//     });
+// });
 
 $('#sendAssessButton').click(function (event) {
     console.log('submittt!!');
@@ -126,3 +126,31 @@ function emailValidations(emails) {
     }
     return true;
 }
+
+function displayEmail() {
+
+    let due = $('#inputDateBox').val();
+    let time = '08:00 AM';
+
+    let message = 'Welcome,\n\n' + 
+    'We have kindly created an account for you, and with this account ' +
+    'you can access your assessment form.\n\n' +
+    'Please submit your solutions by the DUE DATE: ' + 
+    '  ' + due + '  ' + time + '\n\n' +
+    'Your Account name is your email address and the default initial password ' +
+    'is attached below. Really recommend to update your Password after logging in.\n\n' +
+    'User name: <EMAIL_ADDRESS> \n' +
+    'Password: ********* \n\n' +
+    'Please click the link below to login to your account.\n\n' +
+    '\" THE LINK TO LOGIN TO HIRE ME CODER \"\n\n' +
+    'Best regards,\n' +
+    'MapTek Team';
+    
+    $('#emailMessage').val(message);
+    document.getElementById('emailMessage').disabled = false;
+    // $('#emailMessage').disabled(false);
+}
+
+$('#inputTimeBox').datetimepicker({
+    format: 'LT'
+});
