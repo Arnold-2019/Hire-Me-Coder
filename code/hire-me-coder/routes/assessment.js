0e3cb122fc27a946870883944fdaacd1b4a66440
Ying Wang <ying.wang01@student.adelaide.edu.au>
2019-09-25 20:51:52 +0930

    complete create account for candidate, send link to candidate

--- code/hire-me-coder/routes/assessment.js
+++ code/hire-me-coder/routes/assessment.js
@@ -9,6 +9,11 @@ const db = fb.firestore();
 router.get('/send-assessment', function(req, res, next) {
     res.render('assessment');
     console.log('-----TEST LINE---- 1');
+    db.collection('candidate_users').get().then(docs => {
+        docs.forEach(doc => {
+            console.log(doc.data().email);
+        })
+    })
 });
 
 router.get('/preview', function(req, res) {
@@ -19,7 +24,7 @@ router.get('/preview', function(req, res) {
 
 router.use(bodyParser.urlencoded({extended: true})); 
 router.post('/send-assessment', function (req, res, next) {
-    console.log('-----TEST LINE---- 3');
+    console.log('-----TEST LINE---- 2');
     var sender = 'noreply@maptek.com';
     var numSentMails = 0;
     var mails = req.body.emails;
@@ -45,37 +50,61 @@ router.post('/send-assessment', function (req, res, next) {
             pass: 'Sep_group5'
         }
     });
-
+      
     for (var i = 0; i < numMails; i++) {
-        var mailOptions = {
-            from: sender,
-            to: maillist[i],
-            subject: 'MapTek Invitation!',
-            text: 'Wellcome,\n\n' + 
-                  'We have kindly created an account for you, and with this account ' +
-                  'you can access your assessment form.\n\n' +
-                  'Please submit your solutions by the DUE DATE: ' + 
-                  '  ' + due + '  ' + time + '\n\n' +
-                  'Your Account name is your email address and the defult initial password ' +
-                  'is attached below. Realy recommend to update your Password after logging in.\n\n' +
-                  'User name: ' + maillist[i] + '\n' +
-                  'Password: ' + passwords[i] + '\n\n' +
-                  'Please click the link below to login to your account.\n\n' +
-                  '\" THE LINK TO LOGIN TO HIRE ME CODER \"\n\n' +
-                  'Best regards,\n' +
-                  'MapTek Team'
-        };
+        var emailAddress = maillist[i];
+        var passWord = passwords[i];
 
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(numSentMails);
-            } else {
-                numSentMails += 1;
-                console.log('Number of mails sent: ' + numSentMails + ' !');
-            }
-        });
+        fb.auth().createUser({
+            email: emailAddress,
+            password: passWord,
+            displayName: ""
+        }).then(cred => {
+            var candidateRef = db.collection('candidate_users').doc(cred.uid);
+            candidateRef.set({
+                firstName: '',
+                lastName: '',
+                email: emailAddress,
+                testName: test,
+                dueDate: due,
+                dueTime: time,
+                isActive: false
+            }).then(() => {
+                var mailOptions = {
+                    from: sender,
+                    to: emailAddress,
+                    subject: 'MapTek Invitation!',
+                    text: 'Wellcome,\n\n' + 
+                          'We have kindly created an account for you, and with this account ' +
+                          'you can access your assessment form.\n\n' +
+                          'Please submit your solutions by the DUE DATE: ' + 
+                          '  ' + due + '  ' + time + '\n\n' +
+                          'Your Account name is your email address and the defult initial password ' +
+                          'is attached below. Realy recommend to update your Password after logging in.\n\n' +
+                          'User name: ' + emailAddress + '\n' +
+                          'Password: ' + passWord + '\n\n' +
+                          'Please click the link below to login to your account.\n\n' +
+                          'https://hireme-coder.firebaseapp.com/login\n\n' +
+                          'Best regards,\n' +
+                          'MapTek Team'
+                };
+        
+                transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                        console.log(numSentMails);
+                    } else {
+                        numSentMails += 1;
+                        console.log('Number of mails sent: ' + numSentMails);
+                    }
+                });
+                res.sendStatus(200);
+            }).catch(err => {
+                res.sendStatus(500, {error: err});
+            })
+        }).catch(err => {
+            console.log(err.message);
+        })
     }
-    res.send(numSentMails + ' mails have been sent.');
 });
 
 router.get('/test/view', function (req, res, next) {
