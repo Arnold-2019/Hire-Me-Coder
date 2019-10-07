cd931ebc72f702c7cfaceed81d6e8d42cdb39688
Ying Wang <ying.wang01@student.adelaide.edu.au>
2019-10-07 11:40:20 +1030

    date check is completed

--- code/hire-me-coder/routes/assessment.js
+++ code/hire-me-coder/routes/assessment.js
@@ -1,6 +1,5 @@
 /* Remain issues: 26/09/2019 2:05 pm
    1. there is a bug in sending multiple emails
-   2. cannot get the correct parameters format of 'new Date()' function
 */
 
 const express = require('express');
@@ -37,7 +36,7 @@ router.post('/send-assessment', function (req, res, next) {
     var due = req.body.dueDate;
     var time = req.body.dueTime;
     var test = req.body.testName;
-    console.log(mails, due, test);
+    console.log(mails, due, time, test);
 
     var maillist = mails.split(",");
     var numMails = maillist.length;
@@ -67,71 +66,21 @@ router.post('/send-assessment', function (req, res, next) {
                 console.log(emailAddress + ' is existing!');
                 isActive(emailAddress).then(act => {
                     if (act) {
-                        console.log(emailAddress + ': is active!')
+                        console.log(emailAddress + ' has in progressing assessment. (is active)')
                     } else {
-                        console.log(emailAddress + ': is NOT active!')
+                        console.log(emailAddress + ': is NOT active!');
+                        updateUser(emailAddress, due, time, test);
                         sendEmail(emailAddress,passWord,due, time);
                         res.sendStatus(200);
                     }
                 })
             } else {
-                console.log(emailAddress + ': was created!')
+                console.log(emailAddress + ': was created!');
                 createCandidate(emailAddress, passWord, due, time, test);
                 sendEmail(emailAddress, passWord, due, time);
                 res.sendStatus(200);
             }
         });
-
-        // fb.auth().createUser({
-        //     email: emailAddress,
-        //     password: passWord,
-        //     displayName: ""
-        // }).then(cred => {
-        //     var candidateRef = db.collection('candidate_users').doc(cred.uid);
-        //     candidateRef.set({
-        //         firstName: '',
-        //         lastName: '',
-        //         testName: test,
-        //         email: emailAddress,
-        //         dueDate: due,
-        //         dueTime: time,
-        //         // isActive: true,
-        //         isSubmit: false
-        //     }).then(() => {
-        //         var mailOptions = {
-        //             from: sender,
-        //             to: emailAddress,
-        //             subject: 'MapTek Invitation!',
-        //             text: 'Wellcome,\n\n' + 
-        //                   'We have kindly created an account for you, and with this account ' +
-        //                   'you can access your assessment form.\n\n' +
-        //                   'Please submit your solutions by the DUE DATE: ' + 
-        //                   '  ' + due + '  ' + time + '\n\n' +
-        //                   'Your Account name is your email address and the defult initial password ' +
-        //                   'is attached below. Realy recommend to update your Password after logging in.\n\n' +
-        //                   'User name: ' + emailAddress + '\n' +
-        //                   'Password: ' + passWord + '\n\n' +
-        //                   'Please click the link below to login to your account.\n\n' +
-        //                   'https://hireme-coder.firebaseapp.com/login\n\n' +
-        //                   'Best regards,\n' +
-        //                   'MapTek Team'
-        //         };
-        
-        //         transporter.sendMail(mailOptions, function(error, info){
-        //             if (error) {
-        //                 console.log(numSentMails);
-        //             } else {
-        //                 numSentMails += 1;
-        //                 console.log('Number of mails sent: ' + numSentMails);
-        //             }
-        //         });
-        //         res.sendStatus(200);
-        //     }).catch(err => {
-        //         res.sendStatus(500, {error: err});
-        //     })
-        // }).catch(err => {
-        //     console.log(err.message);
-        // })
     }
 });
 
@@ -158,16 +107,12 @@ async function isActive(userEmail) {
     await db.collection('candidate_users').get().then(docs => {
         docs.forEach(doc => {
             if (doc.data().email === userEmail) {
-                // var due = doc.data().dueDate;
-                // var time = doc.data().dueTime.substring(0,4);
-                // var formatDate = due + ' ' + time + ' am';
-                // console.log('due: ' + due);
-                // console.log('time: ' + time);
-                // console.log('formatDate: ' + formatDate);
-                
-                var dueTime = new Date('December 17, 2018 03:24:00');
+                var due = doc.data().dueDate;
+                var time = doc.data().dueTime;
+                dueTime = formatTime(due, time);
+                var dueTime = new Date(dueTime);
                 var currentTime = new Date();
-                console.log('dueTime: ' + dueTime);
+                console.log('expiryTime: ' + dueTime);
                 console.log('curTime: ' + currentTime);
                 
                 if (dueTime.getTime() > currentTime.getTime() && !doc.data().isSubmit) {
@@ -183,6 +128,60 @@ async function isActive(userEmail) {
     } else {
         return false;
     }
+}
+
+async function updateUser(userEmail, due, time, test) {
+    await db.collection('candidate_users').get().then(docs => {
+        docs.forEach(document => {
+            if (document.data().email === userEmail) {
+                // cannot get the doc and update info.
+                db.collection('candidate_users').doc(document.uid).update({
+                    dueDate: due,
+                    dueTime: time,
+                    testName: test
+                });
+                console.log('Data base has been updated!!!!');
+                return
+            }
+        });
+    });
+}
+
+function formatTime(date, time) {
+    dateArr = date.split('/')
+    d = dateArr[0]
+    m = dateArr[1]
+    y = dateArr[2]
+
+    timeArr = time.split(' ')
+    time = timeArr[0]
+    time_arr = time.split(':')
+    hour = time_arr[0]
+    minute = time_arr[1]
+    switch (timeArr[1]) {
+        case 'PM':
+            hour = (Number(hour) + 12).toString(10)
+    }
+    time = hour + ':' + minute + ':00'
+
+    month = ''
+    switch (m) {
+        case '01': month = 'January'; break;
+        case '02': month = 'February'; break;
+        case '03': month = 'March'; break;
+        case '04': month = 'April'; break;
+        case '05': month = 'May'; break;
+        case '06': month = 'June'; break;
+        case '07': month = 'July'; break;
+        case '08': month = 'August'; break;
+        case '09': month = 'September'; break;
+        case '10': month = 'October'; break;
+        case '11': month = 'November'; break;
+        case '12': month = 'Desember'
+    }
+
+    dueTime = month + ' ' + d + ', ' + y + ' ' + time
+    return dueTime
 }
 
 router.get('/test/view', function (req, res, next) {
